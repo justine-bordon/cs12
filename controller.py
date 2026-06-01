43a2e1001c8dab57260f33d16e973bc0183d824a
justine-bordon <jtbordon@up.edu.ph>
2026-06-01 13:48:08 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -22,7 +22,7 @@ class GameController:
                 model.shoot()
         else:
             if not model.game_over:
-                model.round_start(view.input_space())
+                model.round_start(view.input_leftclick())
 
         model.update()
 
